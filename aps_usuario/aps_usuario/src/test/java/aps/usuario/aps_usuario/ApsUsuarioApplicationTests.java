18812f03448db8658fcfd3e0be191f9383cf387e
Mateus Porto <mateusppp96@gmail.com>
2025-03-10 11:44:50 -0300

    inicio2

---
+++ aps_usuario/aps_usuario/src/test/java/aps/usuario/aps_usuario/ApsUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.usuario.aps_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
